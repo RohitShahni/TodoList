44189d562de694788fecb713ee444283a46b02ae
rohit <rohitshahni121@gmail.com>
2023-10-06 02:46:22 +0530

    edit functionality created

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './TodoItem.module.css'
 
-const TodoItem = () => {
+const TodoItem = (props) => {
+    console.log('props=========', props)
+
+    const { item, removeTodo, index, editHandleChange } = props;
+    const [edit, setEdit] = useState(Boolean)
+    const [check, setCheck] = useState(Boolean)
+    const [isChecked, setIsChecked] = useState(false);
+    let disabled = "disabled";
+    let enabled = !disabled;
+
+
+    const editHandler = (Boolean) => {
+        setEdit(Boolean)
+        disabled = enabled;
+    }
+    const saveHandler = (Boolean) => {
+        setEdit(Boolean)
+    }
+
+    const toggleCheckbox = (index) => {
+        setIsChecked(!isChecked);
+    };
+
     return (
 
         <div className={s.inputContainer}>
-            <div className={s.round}>
-                <input type="checkbox" checked id="checkbox" />
+            <div className={s.round} onClick={()=>toggleCheckbox(index)}>
+                <input type="checkbox" checked={isChecked} id="checkbox" />
                 <label for="checkbox"></label>
             </div>
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="Add Task"
-                    aria-label="Recipient's username" aria-describedby="button-addon2" ></input>
+                    aria-label="Recipient's username" aria-describedby="button-addon2"
+                    disabled={!edit}
+                    value={item && item}
+                    onChange={(e) => editHandleChange(e.target.value, index)}
+                />
             </div>
-            <button class="btn btn-success" type="button" id="button-addon2">Edit</button>
-            <button class="btn btn-danger" type="button" id="button-addon2">Cancel</button>
+            {!edit && <button class="btn btn-success" type="button" id="button-addon2" onClick={() => editHandler(true)}>Edit</button>}
+            {edit && <button class="btn btn-success" type="button" id="button-addon2" onClick={() => saveHandler(false)}>Save</button>}
+            <button class="btn btn-danger" type="button" id="button-addon2" onClick={() => removeTodo(index)}>Delete</button>
         </div>
     )
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -4,18 +4,35 @@ import TodoItem from './TodoItem'
 
 export const TodoList = () => {
     const [inputText, setinputText] = useState('')
-    const [listItem, setListItem] = useState('')
+    const [listItem, setListItem] = useState([])
+    const [editable, setEditable] = useState('')
 
     const handleChange = event => {
         setinputText(event.target.value);
-
-        console.log('value is:', event.target.value);
     };
 
     const setTodoList = () => {
-        console.log({ inputText })
-        setListItem(inputText)
+        if (inputText.trim() !== '') {
+            setListItem([...listItem, inputText]);
+            setinputText('');
+        }
     }
+
+    const editHandleChange = (value, index) => {
+        console.log({ value, index })
+        const updatedList = [...listItem];
+        updatedList[index] = value;
+        setListItem(updatedList);
+    }
+
+    const removeTodo = (index) => {
+        console.log('index========', index)
+        const newList = [...listItem];
+        newList.splice(index, 1);
+        setListItem(newList);
+    }
+
+
 
     return (
         <div className={s.container}>
@@ -30,7 +47,13 @@ export const TodoList = () => {
                 <button class="btn btn-success" type="button" id="button-addon2" onClick={setTodoList}>Add Todo</button>
             </div>
 
-            {listItem && <TodoItem />}
+            {
+                listItem && listItem.map((item, index) => {
+                    return <TodoItem item={item} index={index} removeTodo={removeTodo} handleChange={handleChange} editHandleChange={editHandleChange} />
+
+                })
+            }
+
         </div>
     )
 }
